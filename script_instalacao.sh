395e7712363e60ac3182c4f7319756106ad37ff9
Caio Arnoni <125586491+caioarnonii@users.noreply.github.com>
2023-11-27 10:15:12 -0300

    Update script_instalacao.sh

--- script_instalacao.sh
+++ script_instalacao.sh
@@ -2,6 +2,7 @@
 
 # Verificação do Java
 JAVAVERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+BDPATH="script_streamoon.sql"
 JARPATH="projeto-streamoon.jar"
 
 
@@ -14,6 +15,32 @@ if [[ $JAVAVERSION == "17.0.5" ]];
 else
     sudo apt install openjdk-17-jre -y   
 fi
+
+
+# Verificação do Mysql
+
+if [ ! -e "$BDPATH" ];
+    then
+        echo "Baixando Banco de Dados"
+        wget https://raw.githubusercontent.com/PI-Streamoon/B-Streamoon/main/script_streamoon.sql
+fi
+
+which docker
+
+if [ $? != 0 ]
+    then sudo apt install docker.io
+fi
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo docker pull mysql:8.0
+
+sudo docker run -d -p 3306:3306 --name containerBD  -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:8.0
+
+sudo docker exec -i containerBD mysql -h 0.0.0.0 -P 3306 -uroot -purubu100 mysql < script_streamoon.sql
+
 
 
 # Verificação se o .jar já foi instalado
